476726cf9f679c85df9ce19ce1eb6339ee4a0796
Sean McGuire <smcguirenj@gmail.com>
2016-11-14 17:13:53 -0500

    Actually made a bot

--- NodejsConsoleApp1/NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/NodejsConsoleApp1/app.js
@@ -1,1 +1,17 @@
-﻿console.log('Hello world');
+var Discord = require("discord.js");
+
+var bot = new Discord.Client();
+bot.on("ready", () =>
+{
+    console.log(bot.user)
+});
+bot.on("message", (message) =>
+{
+    if (message.contains == "!clear") {
+        if (message.author.id == "132902891819237377") {
+            message.channel.bulkDelete(message.channel.fetchMessages().findAll("author", bot.users.find("id", "132902891819237377")));
+        }
+    }
+})
+
+bot.login("")

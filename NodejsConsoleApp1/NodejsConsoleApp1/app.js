509abecc05f7137cfa6fc1192accffcb1c5f99a2
Sean McGuire <smcguirenj@gmail.com>
2016-11-15 11:40:35 -0500

    Added new function and moved !clear

--- NodejsConsoleApp1/NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/NodejsConsoleApp1/app.js
@@ -5,13 +5,23 @@ bot.on("ready", () =>
 {
     console.log(bot.user)
 });
-bot.on("message", (message) =>
-{
+function patGotGrounded(message) {
+    /* 
+    This code currently needs to tested.
+    I have no idea if this code will actually work so we gotta do that
+    */
     if (message.contains == "!clear") {
         if (message.author.id == "132902891819237377") {
             message.channel.bulkDelete(message.channel.fetchMessages().findAll("author", bot.users.find("id", "132902891819237377")));
         }
     }
+}
+function chatSensor (message) {
+    //FINISH THIS AT A LATER TIME OK XD
+}
+bot.on("message", (message) =>
+{
+    patGotGrounded(message);
 })
 
-bot.login("")
+bot.login("") // ADD THIS LOCALLY

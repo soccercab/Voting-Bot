d088efa94de9c9d219390246ef3e05a777955296
Sean McGuire <smcguirenj@gmail.com>
2017-11-29 11:02:33 -0500

    Update create.py

--- create.py
+++ create.py
@@ -3,4 +3,9 @@ import os
 import time
 print "Installing..."
 urllib.urlretrieve ("https://github.com/SPMNJ/Currentbot/raw/master/install.py", "install.py")
-os.startfile("install.py")
+text = ['set WshShell=createobject("wscript.shell")', 'WScript.Sleep 1000', 'WshShell.run "install.py"', '']
+code = open("run.vbs","w+")
+for i in range(len(text)):
+    code.write(text[i]+"\n")
+code.close()
+os.startfile("run.vbs")

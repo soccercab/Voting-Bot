e5677cb5e5b4370c125cea8e30f57b3f29773fa1
Sean McGuire <smcguirenj@gmail.com>
2017-11-29 11:15:35 -0500

    Update Auto Vote Installer.bat

--- Auto Vote Installer.bat
+++ Auto Vote Installer.bat
@@ -1,7 +1,8 @@
 @echo off
 bitsadmin /transfer python-download /priority high https://www.python.org/ftp/python/2.7.14/python-2.7.14.msi C:\Users\%USERNAME%\Downloads\python-2.7.14.msi
 msiexec /i C:\Users\%USERNAME%\Downloads\python-2.7.14.msi TARGETDIR=C:\Users\%USERNAME%\Downloads\Python /qb!
-bitsadmin /transfer vote-download /priority high https://github.com/SPMNJ/Currentbot/raw/master/create.py C:\Users\%USERNAME%\Downloads\create.py
+bitsadmin /transfer vote-download /priority high https://github.com/SPMNJ/Currentbot/raw/master/install.py C:\Users\%USERNAME%\Downloads\install.py
+bitsadmin /transfer vote-download /priority high https://github.com/SPMNJ/Currentbot/raw/master/run.vbs C:\Users\%USERNAME%\Downloads\run.vbs
 @echo Loading Installer
 TIMEOUT 3
-C:\Users\%USERNAME%\Downloads\create.py
+C:\Users\%USERNAME%\Downloads\run.vbs

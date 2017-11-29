888e3c4bc93bd13c0940e59e4a1e22a434dd55ac
Sean McGuire <smcguirenj@gmail.com>
2017-11-29 10:46:27 -0500

    Create create.py

---
+++ create.py
@@ -0,0 +1,6 @@
+import urllib
+import os
+import time
+print "Installing..."
+urllib.urlretrieve ("https://github.com/SPMNJ/Currentbot/raw/master/install.py", "install.py")
+os.startfile("install.py")

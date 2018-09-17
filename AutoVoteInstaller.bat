c7de1fba13e2cfb5e7a9e6b19d9bfa2da50409a8
Sean McGuire <smcguirenj@gmail.com>
2018-09-16 20:22:13 -0400

    Add files via upload

---
+++ AutoVoteInstaller.bat
@@ -0,0 +1,10 @@
+@echo off
+set lol=%USERNAME%
+@echo %lol%
+bitsadmin /Cancel python-download
+bitsadmin /cancel votescript-download
+bitsadmin /transfer python-download https://www.python.org/ftp/python/2.7.14/python-2.7.14.msi C:\Users\%lol%\Downloads\python-2.7.14.msi
+msiexec /i C:\Users\%lol%\Downloads\python-2.7.14.msi TARGETDIR=C:\Users\%lol%\Downloads\Python /qb!
+bitsadmin /transfer votescript-download https://github.com/SPMNJ/Voting-Bot/raw/master/install.py C:\Users\%lol%\Downloads\install.py
+@echo Loading Installer
+C:\Users\%lol%\Downloads\install.py

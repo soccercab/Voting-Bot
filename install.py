75aa9c08ececd5787141722a91ec1b91ae6dd66a
Sean McGuire <smcguirenj@gmail.com>
2018-09-16 22:17:06 -0400

    Update install.py

--- install.py
+++ install.py
@@ -1,6 +1,8 @@
 import urllib
 import os
 import time
+import site
+reload(site)
 os.chdir(os.path.expanduser("~")+"/Downloads/")
 print "Installing..."
 urllib.urlretrieve ("https://bootstrap.pypa.io/get-pip.py", "runpip.py")

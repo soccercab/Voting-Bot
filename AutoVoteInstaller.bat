6f529df5f518b69a55fc8192ae45f096cfc465e7
soccercab <soccercab@yahoo.com>
2018-09-16 23:44:05 -0400

    Update AutoVoteInstaller.bat

--- AutoVoteInstaller.bat
+++ AutoVoteInstaller.bat
@@ -3,9 +3,10 @@ set lol=%USERNAME%
 @echo Installing for user: %lol%
 bitsadmin /Cancel python-download
 bitsadmin /cancel votescript-download
+bitsadmin /cancel requests-download
 bitsadmin /transfer python-download https://www.python.org/ftp/python/2.7.15/python-2.7.15.msi C:\Users\%lol%\Downloads\python-2.7.15.msi
 msiexec /i C:\Users\%lol%\Downloads\python-2.7.15.msi TARGETDIR=C:\Users\%lol%\Downloads\Python /qb!
-bitsadmin /transfer votescript-download https://github.com/soccercab/Voting-Bot/blob/master/install.py C:\Users\%lol%\Downloads\install.py
-bitsadmin /transfer votescript-download https://github.com/soccercab/Voting-Bot/blob/master/Requests.bat C:\requests.bat
+bitsadmin /transfer votescript-download https://github.com/soccercab/Voting-Bot/raw/master/install.py C:\Users\%lol%\Downloads\install.py
+bitsadmin /transfer requests-download https://github.com/soccercab/Voting-Bot/raw/master/requests.bat C:\requests.bat
 @echo Loading Installer
 C:\Users\%lol%\Downloads\install.py
